df4fc9b641e64d2be10784530fa5f4b5c6c91749
cas <cas@interglobal.vision>
2016-12-20 17:23:43 -0600

    add packages and stuff

--- functions.php
+++ functions.php
@@ -50,7 +50,6 @@ register_nav_menus( array(
 function cmb_initialize_cmb_meta_boxes() {
   if (!class_exists( 'cmb2_bootstrap_202' ) ) {
     require_once 'vendor/webdevstudios/cmb2/init.php';
-    require_once 'vendor/webdevstudios/cmb2-post-search-field/lib/init.php';
   }
 }
 add_action( 'init', 'cmb_initialize_cmb_meta_boxes', 11 );

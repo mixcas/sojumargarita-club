9024687a802964408c0068dc692185bd4dea67dc
cas <cas@interglobal.vision>
2016-12-20 18:38:24 -0600

    add show metaboxes

--- lib/meta-boxes.php
+++ lib/meta-boxes.php
@@ -2,17 +2,17 @@
 
 /* Get post objects for select field options */
 function get_post_objects( $query_args ) {
-$args = wp_parse_args( $query_args, array(
+  $args = wp_parse_args( $query_args, array(
     'post_type' => 'post',
-) );
-$posts = get_posts( $args );
-$post_options = array();
-if ( $posts ) {
+  ) );
+  $posts = get_posts( $args );
+  $post_options = array();
+  if ( $posts ) {
     foreach ( $posts as $post ) {
-        $post_options [ $post->ID ] = $post->post_title;
+      $post_options [ $post->ID ] = $post->post_title;
     }
-}
-return $post_options;
+  }
+  return $post_options;
 }
 
 
@@ -38,6 +38,61 @@ function igv_cmb_metaboxes() {
 	 * Metaboxes declarations here
    * Reference: https://github.com/WebDevStudios/CMB2/blob/master/example-functions.php
 	 */
+
+
+
+  $show_meta = new_cmb2_box( array(
+    'id'            => $prefix . 'show_meta',
+    'title'         => esc_html__( 'Show Metadata', 'cmb2' ),
+    'object_types'  => array( 'show', ), // Post type
+    // 'show_on_cb' => 'yourprefix_show_if_front_page', // function should return a bool value
+    // 'context'    => 'normal',
+    'priority'   => 'high',
+    // 'show_names' => true, // Show field names on the left
+    // 'cmb_styles' => false, // false to disable the CMB stylesheet
+    // 'closed'     => true, // true to keep the metabox closed by default
+    // 'classes'    => 'extra-class', // Extra cmb2-wrap classes
+    // 'classes_cb' => 'yourprefix_add_some_classes', // Add classes through a callback.
+  ) );
+
+  $show_meta->add_field( array(
+    'name' => __( 'Youtube playlist ID', 'cmb2' ),
+    'id'   => $prefix . 'playlist_id',
+    'type' => 'text',
+  ));
+  
+  $show_meta->add_field( array(
+    'name' => __( 'Tracklist', 'cmb2' ),
+    'id'   => $prefix . 'tracklist',
+    'type' => 'wysiwyg',
+  ));
+
+  $show_meta->add_field( array(
+    'name' => __( 'Episode #', 'cmb2' ),
+    'id'   => $prefix . 'episode_number',
+    'type' => 'text',
+    'attributes' => array(
+      'type' => 'number',
+      'pattern' => '\d*',
+    ),
+    'sanitization_cb' => 'absint',
+    'escape_cb'       => 'absint',
+  ));
+
+
+  $show_meta->add_field( array(
+    'name'    => 'Cover photo',
+    'desc'    => 'Upload an image or enter an URL.',
+    'id'   => $prefix . 'cover_photo',
+    'type'    => 'file',
+    // Optional:
+    'options' => array(
+      'url' => false, // Hide the text input for the url
+    ),
+    'text'    => array(
+      'add_upload_file_text' => 'Add Cover Photo' // Change upload button text. Default: "Add or Upload File"
+    ),
+  ) );
 
 }
 ?>

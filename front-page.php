2c3d9022bd58a4ed8c77389d280afaaf46d31fa2
cas <cas@interglobal.vision>
2017-02-28 20:37:15 -0600

    add big posts to front-page

--- front-page.php
+++ front-page.php
@@ -14,37 +14,41 @@ get_header();
   ));
 if( $latest_posts->have_posts() ) {
 ?>
-  <section id="noticias" class="noticias-archive padding-top-basic padding-bottom-basic text-main-color background-second-color">
-    <div class="container">
+  <section id="noticias" class="noticias-archive text-main-color background-second-color">
 
 
 <?php
   while( $latest_posts->have_posts() ) {
     $latest_posts->the_post();
 ?>
-
-        <article <?php post_class('grid-row justify-center margin-bottom-basic'); ?> id="post-<?php the_ID(); ?>">
-          <div class="grid-item item-s-12 item-m-10 text-align-center">
-            <a href="<?php the_permalink() ?>"><h2 class="margin-bottom-small font-size-h1 text-fourth-color"><?php the_title(); ?></h2></a>
+    <a href="<?php the_permalink(); ?>">
+      <article <?php post_class('text-second-color'); ?> id="post-<?php the_ID(); ?>">
+        <div class="cover-photo-holder grid-row justify-center align-items-center">
+          <div class="cover-photo" style="background-image: url(<?php the_post_thumbnail_url(); ?>)"></div>
+          <div class="grid-item item-s-12 item-l-9 text-align-center">
+            <h1 class="margin-top-tiny margin-bottom-basic drop-shadow font-larger"><?php the_title(); ?></h1>
           </div>
+        </div>
+      </article>
+    </a>
 
-          <div class="grid-item item-s-12 item-l-9 content-copy">
-            <?php the_post_thumbnail() ?>
-
-            <?php the_content(); ?>
-          </div>
-
-        </article>
 
 <?php
+    $next_page = get_next_posts_link();
   }
 ?>
     </div>
+<?php
+  if ($next_page) {
+?>
     <div class="grid-row justify-center">
       <div class="grid-item item-s-8 text-align-center">
       <a class="font-bold font-italic" href="<?php echo home_url('/noticias/page/2'); ?>">Más Noticias</a>
       </div>
     </div>
+<?php
+  }
+?>
   </section>
 <?php
 }

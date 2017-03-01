ebaf2ab73a64a15f5bcd0d82006d94e63edff9b4
cas <cas@interglobal.vision>
2017-02-28 18:50:27 -0600

    fix fb code

--- partials/header-scripts.php
+++ partials/header-scripts.php
@@ -4,26 +4,26 @@ $facebook_app_id = IGV_get_option('_igv_site_options', '_igv_og_fb_app_id');
 
 if (!empty($facebook_app_id)) {
 ?>
-  <script>
-  window.fbAsyncInit = function() {
-    console.log('fb init');
-    FB.init({
-    appId      : '<?php echo $facebook_app_id; ?>',
-      xfbml      : true,
-      version    : 'v2.8'
-});
-FB.AppEvents.logPageView();
-};
+t
+<script>
+	window.fbAsyncInit = function() {
+		FB.init({
+      appId      : '<?php echo $facebook_app_id; ?>',
+			cookie     : true,
+			xfbml      : true,
+			version    : 'v2.8'
+		});
+		FB.AppEvents.logPageView();
+	};
 
-(function(d, s, id){
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-js.src = "//connect.facebook.net/en_US/sdk.js";
-fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+	(function(d, s, id){
+		 var js, fjs = d.getElementsByTagName(s)[0];
+		 if (d.getElementById(id)) {return;}
+		 js = d.createElement(s); js.id = id;
+		 js.src = "//connect.facebook.net/en_US/sdk.js";
+		 fjs.parentNode.insertBefore(js, fjs);
+	 }(document, 'script', 'facebook-jssdk'));
 </script>
-
 <?php
 }
 ?>

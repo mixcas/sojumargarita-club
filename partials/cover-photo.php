3e6d1c3d83b8e31a73178cf06a2b4dc062af9e05
cas <cas@interglobal.vision>
2017-03-01 11:11:30 -0600

    fix home titles position

--- partials/cover-photo.php
+++ partials/cover-photo.php
@@ -1,9 +1,9 @@
 <?php
-$align_items_center = is_home();
+$align_items_center = is_front_page();
 ?>
   <div class="cover-photo-holder grid-row justify-center <?php echo $align_items_center == true ? 'align-items-center' : 'align-items-end'; ?>" style="background-image: url(<?php the_post_thumbnail_url(); ?>)">
-        <div class="cover-photo"></div>
-        <div class="grid-item item-s-12 item-l-9 text-align-center">
-          <h1 class="margin-top-tiny margin-bottom-basic drop-shadow font-larger"><span class="highlight"><?php the_title(); ?></span></h1>
-        </div>
-      </div>
+    <div class="cover-photo"></div>
+    <div class="grid-item item-s-12 item-l-9 text-align-center">
+      <h1 class="margin-top-tiny margin-bottom-basic drop-shadow font-larger"><span class="highlight"><?php the_title(); ?></span></h1>
+    </div>
+  </div>

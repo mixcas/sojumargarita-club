2a848e0908170e71fdb71c4d82a4c49dd05f1b3e
cas <cas@interglobal.vision>
2017-02-28 20:14:20 -0600

    add soundcloud link

--- header.php
+++ header.php
@@ -51,6 +51,7 @@ get_template_part('partials/seo');
             <a class="social-icon" href="https://www.youtube.com/channel/UCXxiO5-SXm-1or2dcB-4qXQ" target="_blank" rel="nofollow"><?php echo url_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/youtube_logo.svg'); ?></a>
             <a class="social-icon" href="http://facebook.com/sojumargaritaclub/" target="_blank" rel="nofollow"><?php echo url_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/fb_logo.svg'); ?></a>
             <a class="social-icon" href="https://open.spotify.com/user/sojumargaritaclub/playlist/2yPxW7XUH6jhNXgqYZQWKY" target="_blank" rel="nofollow"><?php echo url_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/spotify_logo.svg'); ?></a>
+            <a class="social-icon" href="https://soundcloud.com/sojumargaritaclub" target="_blank" rel="nofollow"><?php echo url_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/soundcloud_logo.svg'); ?></a>
           </div>
         </div>
       </div>

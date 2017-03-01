59e4ae2c55217479c27a1138a0532947d56eb79a
cas <cas@interglobal.vision>
2017-02-28 21:02:07 -0600

    fix l-4 image size

--- lib/thumbnail-sizes.php
+++ lib/thumbnail-sizes.php
@@ -8,7 +8,7 @@ if( function_exists( 'add_image_size' ) ) {
   add_image_size( 'admin-thumb', 150, 150, false );
   add_image_size( 'opengraph', 1200, 630, true );
 
-  add_image_size( 'l-4', 534, 576, false );
+  add_image_size( 'l-4', 264, 264, false );
   add_image_size( 'l-10', 1300, 9999, false );
   add_image_size( 'l-3', 400, 432, false );
 

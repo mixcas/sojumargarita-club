3fbf3f52fd7f1659923b9b3c35b2bbee14133289
cas <cas@interglobal.vision>
2017-03-01 10:37:25 -0600

    add noticias archive on index

--- index.php
+++ index.php
@@ -3,44 +3,29 @@ get_header();
 ?>
 
 <main id="main-content">
-  <section id="posts">
-    <div class="container">
-      <div class="grid-row justify-center">
-
 <?php
 if( have_posts() ) {
+?>
+  <section id="noticias" class="noticias-archive text-main-color background-second-color">
+<?php
   while( have_posts() ) {
     the_post();
 ?>
+    <a href="<?php the_permalink(); ?>">
+      <article <?php post_class('text-second-color'); ?> id="post-<?php the_ID(); ?>">
+    <?php get_template_part('partials/cover-photo'); ?>
+      </article>
+    </a>
 
-        <article <?php post_class('grid-row justify-center margin-bottom-basic text-second-color'); ?> id="post-<?php the_ID(); ?>">
-          <div class="grid-item item-s-6 text-align-center">
-            <a href="<?php the_permalink() ?>"><h2 class="margin-bottom-small font-size-h1"><?php the_title(); ?></h2></a>
-          </div>
-
-          <div class="grid-item item-s-8 content-copy">
-            <?php the_post_thumbnail() ?>
-
-            <?php the_content(); ?>
-          </div>
-
-        </article>
 
 <?php
   }
 } else {
 ?>
-        <article class="u-alert grid-item item-s-12"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
+    <article class="u-alert grid-item item-s-12"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
 <?php
 } ?>
-
-      </div>
-      <div class="grid-row justify-center">
-        <div class="grid-item item-s-6">
-          <?php get_template_part('partials/pagination'); ?>
-        </div>
-      </div>
-    </div>
+  <?php get_template_part('partials/pagination'); ?>
   </section>
 
 
--- partials/pagination.php
+++ partials/pagination.php
@@ -1,22 +1,40 @@
 <?php
 if( get_next_posts_link() || get_previous_posts_link() ) {
 ?>
+<nav id="pagination" class="background-fourth-color padding-top-basic padding-bottom-basic text-second-color">
+  <div class="grid-row justify-center container font-larger">
   <!-- post pagination -->
-  <nav id="pagination">
 <?php
-$previous = get_previous_posts_link('Newer');
-$next = get_next_posts_link('Older');
-if ($previous) {
-  echo $previous;
-}
-if ($previous && $next) {
-  echo ' &mdash; ';
-}
+$previous = get_previous_posts_link('<<');
+$next = get_next_posts_link('>>');
+
 if ($next) {
-  echo $next;
+?>
+    <div class="grid-item item-s-4 font-larger text-align-right">
+      <?php echo $next; ?>
+    </div>
+<?php
+}
+
+if ($previous && $next) {
+?>
+    <div class="grid-item item-s-4 font-larger">
+      <?php echo ' &mdash; '; ?>
+    </div>
+<?php
+}
+
+if ($previous) {
+?>
+    <div class="grid-item item-s-4 font-large text-align-left">
+      <?php echo $previous; ?>
+    </div>
+<?php
 }
 ?>
-  </nav>
+    </div>
+  </div>
+</nav>
 <?php
 }
 ?>

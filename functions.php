f8d5420e01d5672bb14f8f55b21ef2c735a4076d
cas <cas@interglobal.vision>
2017-03-02 19:01:14 -0600

    add stylesheet version

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@ function scripts_and_styles_method() {
   wp_enqueue_script( 'myscripts', $myscripts,'','',true);
 
   // enqueue stylesheet here. file does not exist until stylus file is processed
-  wp_enqueue_style( 'site', get_stylesheet_directory_uri() . '/css/site.css' );
+  wp_enqueue_style( 'site', get_stylesheet_directory_uri() . '/css/site.css?v=1' );
 
   // dashicons for admin
   if(is_admin()){

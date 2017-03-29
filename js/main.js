b5e4f86f2501c0db139758d2196f0f744cea905e
cas <cas@interglobal.vision>
2017-03-29 15:23:50 -0700

    fix js lint

--- js/main.js
+++ js/main.js
@@ -88,6 +88,6 @@ Site.Share = {
     window.open("https://twitter.com/intent/tweet?url="+encodeURIComponent(document.URL)+"&text="+encodeURIComponent(document.title)+ "&count=none/", "", "height=300, width=550, resizable=1");
     return true;
   }
-}
+};
 
 Site.init();
